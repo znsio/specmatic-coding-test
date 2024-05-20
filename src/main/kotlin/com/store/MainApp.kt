f2a2b91f0e372f8d15383444bbe050a42f7e0b2c
Yogesh Ananda Nikam <yogeshnikam671@gmail.com>
2024-05-20 15:00:54 +0530

    Fix pom file to show correct number of test failures

--- src/main/kotlin/com/store/MainApp.kt
+++ src/main/kotlin/com/store/MainApp.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Application
+open class Application
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
