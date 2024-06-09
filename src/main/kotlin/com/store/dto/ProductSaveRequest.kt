139e5aee2803e67925138f97e091c12becdc2801
sanjeev113f <sanjeevk@sahaj.ai>
2024-06-10 00:12:57 +0530

    Add @JsonSetter annotation to cost property for custom deserialization - Format the file

--- src/main/kotlin/com/store/dto/ProductSaveRequest.kt
+++ src/main/kotlin/com/store/dto/ProductSaveRequest.kt
@@ -1,28 +1,42 @@
 package com.store.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonSetter
 import com.store.domain.Product
 import com.store.enums.ProductType
 import javax.validation.constraints.Pattern
 
 data class ProductSaveRequest(
     @field:Pattern(regexp = "^[a-zA-Z]+$")
-    @JsonProperty("name") val name: String,
+    @JsonProperty("name")
+    val name: String,
     @field:Pattern(regexp = "^[a-zA-Z]+$")
-    @JsonProperty("type") val type: String,
+    @JsonProperty("type")
+    val type: String,
     @field:Pattern(regexp = "^[0-9]+$")
-    @JsonProperty("inventory") val inventory: String,
+    @JsonProperty("inventory")
+    val inventory: String,
+    @field:Pattern(regexp = "^[0-9]+$")
+    @JsonProperty("cost")
+        @JsonSetter(value = "0")
+    private var cost: String?
 ) {
     init {
         validateType(type)
         validateName(name)
     }
 
+    @JsonSetter("cost")
+    fun setCost(cost: String?) {
+        this.cost = cost ?: throw IllegalArgumentException("Invalid input format")
+    }
+
     fun toProject(): Product {
         return Product(
             name = name,
             type = type,
-            inventory = inventory.toInt()
+            inventory = inventory.toInt(),
+            cost = cost?.toInt() ?: 0
         )
     }
 
@@ -31,8 +45,7 @@ data class ProductSaveRequest(
             if (!ProductType.values().any { it.value == type }) {
                 throw IllegalArgumentException(
                     "Invalid product type: $type. Must be one of ${
-                        ProductType.values()
-                            .joinToString(", ")
+                        ProductType.values().joinToString(", ")
                     }"
                 )
             }
