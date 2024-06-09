0f1bccede74d0857af513c56f07df94d2dfcc8b6
sanjeev113f <sanjeevk@sahaj.ai>
2024-06-09 14:00:41 +0530

    Add toProject and validate functions in ProductSaveRequest dto

--- src/main/kotlin/com/store/dto/ProductSaveRequest.kt
+++ src/main/kotlin/com/store/dto/ProductSaveRequest.kt
@@ -1,9 +1,45 @@
 package com.store.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.store.domain.Product
+import com.store.enums.ProductType
+import javax.validation.constraints.Pattern
 
-data class ProductSaveRequest (
+data class ProductSaveRequest(
+    @field:Pattern(regexp = "^[a-zA-Z]+$")
     @JsonProperty("name") val name: String,
+    @field:Pattern(regexp = "^[a-zA-Z]+$")
     @JsonProperty("type") val type: String,
-    @JsonProperty("inventory") val inventory: Int,
-)
+    @field:Pattern(regexp = "^[0-9]+$")
+    @JsonProperty("inventory") val inventory: String,
+) {
+    init {
+        validateType(type)
+        validateName(name)
+    }
+
+    fun toProject(): Product {
+        return Product(
+            name = name,
+            type = type,
+            inventory = inventory.toInt()
+        )
+    }
+
+    companion object {
+        fun validateType(type: String) {
+            if (!ProductType.values().any { it.value == type }) {
+                throw IllegalArgumentException(
+                    "Invalid product type: $type. Must be one of ${
+                        ProductType.values()
+                            .joinToString(", ")
+                    }"
+                )
+            }
+        }
+
+        fun validateName(name: String) {
+            if (name == "true" || name == "false") throw IllegalArgumentException("invalid name")
+        }
+    }
+}
